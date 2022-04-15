d56f47b047ed4eaaf45cd46c2994b56d0b955079
Thekorpy <oscar.korpela@gmail.com>
2022-04-15 17:14:37 +0200

    fully functional lottery deployed to rinkeby

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -20,6 +20,7 @@ contract Lottery is VRFConsumerBase, Ownable {
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
     bytes32 public keyhash;
+    event RequestedRandomness(bytes32 requestId);
 
     constructor(
         address _priceFeedAddress,
@@ -37,8 +38,8 @@ contract Lottery is VRFConsumerBase, Ownable {
 
     function enter() public payable {
         // $50 minimum
-        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         require(lottery_state == LOTTERY_STATE.OPEN, "Lottery isn't open now!");
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
@@ -59,7 +60,8 @@ contract Lottery is VRFConsumerBase, Ownable {
 
     function endLottery() public onlyOwner {
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
-        requestRandomness(keyhash, fee);
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        emit RequestedRandomness(requestId);
     }
 
     function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -6,10 +6,11 @@ from brownie import (
     VRFCoordinatorMock,
     LinkToken,
     Contract,
+    interface,
 )
 
-FORKED_LOCAL_ENVIROMENTS = ["mainnet-fork", "mainnet-fork-dev"]
-LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account(index=None, id=None):
@@ -18,8 +19,8 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     if (
-        network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIROMENTS
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
@@ -43,7 +44,7 @@ def get_contract(contract_name):
         brownie.network.contract.ProjectContract: The most recently deployed version of this contract
     """
     contract_type = contract_to_mock[contract_name]
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         if len(contract_type) <= 0:
             deploy_mocks()
         contract = contract_type[-1]
@@ -65,3 +66,16 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     link_token = LinkToken.deploy({"from": account})
     VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deployed!")
+
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=100000000000000000
+):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
+    tx.wait(1)
+    print("Funded contract!")
+    return tx
